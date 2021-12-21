93012368e27fd66fd1e50b834c741f7819433892
FrankGaribaldi <96451958+FrankGaribaldi@users.noreply.github.com>
2021-12-20 19:13:01 -0500

    Adding CountUpToEvent
    This adds the CountUpToEvent script. This class lets you count to a max number. Once max number is reached, then play a feedback.

---
+++ CountUpToEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Feedbacks;
+
+
+public class CountUpToEvent : MonoBehaviour
+{
+    public int currentNumber = 0;
+    public int maxNumber;
+    public MMFeedbacks EndCountFeedback;
+
+    public void CountUp()
+    {
+        currentNumber++;
+         if(currentNumber == maxNumber)
+        {
+          EndCountFeedback?.PlayFeedbacks();
+        }
+    }
+
+}
